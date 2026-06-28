502943a5e3c8aeffdd5bb04a51ee7063cd4c9fa3
Gudvin122 <skripchukg@gmail.com>
2026-06-28 23:21:31 +0700

    Точка входа.

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -1,2 +1,28 @@
-package example;public class Main {
+package example;
+
+public class Main {
+    public static void sendToMinistryOfEducation(IElectronicJournal journal) {
+        System.out.println("Отправка данных...");
+        System.out.println(journal.getCourseInfo());
+        System.out.println("Статус: " + journal.getPassedStatus());
+    }
+
+    public static void main(String[] args) {
+        ReportCard math = new ReportCard.ReportCardBuilder()
+                .academic_subject("Математика")
+                .grade(3)
+                .build();
+
+        ExamHandler chain = new SubjectValidationHandler();
+        chain.setNext(new GradeValidationHandler())
+                .setNext(new StudentExam(new RegularExam()));
+        chain.handle(math);
+
+        IDecorator decorator = new BasicExam();
+        decorator = new CommentDecorator(decorator);
+        decorator.pass(math);
+
+        new ReportCardProxy(math, "STUDENT").setGrade(5);
+        sendToMinistryOfEducation(new ReportCardAdapter(math));
+    }
 }
